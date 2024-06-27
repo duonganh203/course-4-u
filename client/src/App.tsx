89620c55e41fd0f804607e190552c9296986bbda
unknown <nguyenthuchoang17112003@gmail.com>
2024-06-27 13:31:47 +0700

    Design header for user (only html and css)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,12 @@
-import Counter from "./components/counter";
+import Counter from "./screens/user.screens/counter";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
-import HeaderHomepae from "./components/header";
-import FooterHomepage from "./components/footer";
+import HeaderHomepae from "./components/user.components/header";
 // import "./assets/css/App.css";
 const LayoutUser = () => {
     return (
         <div className='app-container'>
             <HeaderHomepae />
-            {/* <Outlet /> */}
-            <FooterHomepage />
+            <Outlet />
         </div>
     );
 };
@@ -29,3 +27,5 @@ const router = createBrowserRouter([
 function App() {
     return <>{<RouterProvider router={router} />}</>;
 }
+
+export default App;
