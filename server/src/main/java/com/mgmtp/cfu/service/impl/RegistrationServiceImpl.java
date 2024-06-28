0ac195ad9997083c1bbafd3d5864622d9306e733
Duong Anh <rankill52@gmail.com>
2024-06-28 08:34:18 +0700

    MGMIDN-1181: Setup registration service

---
+++ server/src/main/java/com/mgmtp/cfu/service/impl/RegistrationServiceImpl.java
@@ -0,0 +1,21 @@
+package com.mgmtp.cfu.service.impl;
+
+import com.mgmtp.cfu.DTO.RegistrationDTO;
+import com.mgmtp.cfu.entity.Registration;
+import com.mgmtp.cfu.mapper.RegistrationMapper;
+import com.mgmtp.cfu.repository.RegistrationRepository;
+import com.mgmtp.cfu.service.RegistrationService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class RegistrationServiceImpl implements RegistrationService {
+    private final RegistrationRepository registrationRepository;
+
+    @Override
+    public RegistrationDTO getDetailRegistration(Long id) {
+        Registration registration = registrationRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Registration not found"));
+        return RegistrationMapper.toDto(registration);
+    }
+}
