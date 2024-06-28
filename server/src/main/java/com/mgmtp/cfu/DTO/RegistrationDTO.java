0ce21ba67a0646d20b9a1d3688be9ee0d52245ee
Duong Anh <rankill52@gmail.com>
2024-06-28 08:33:53 +0700

    MGMIDN-1181: Setup dto & mapper

---
+++ server/src/main/java/com/mgmtp/cfu/DTO/RegistrationDTO.java
@@ -0,0 +1,23 @@
+package com.mgmtp.cfu.DTO;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.mgmtp.cfu.enums.RegistrationStatus;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
+@FieldDefaults(level= AccessLevel.PRIVATE)
+public class RegistrationDTO {
+    Long id;
+    RegistrationStatus status;
+    LocalDate startDate;
+    LocalDate endDate;
+    Integer score;
+    LocalDate registerDate;
+    Integer duration;
+}
---
+++ server/src/main/java/com/mgmtp/cfu/mapper/RegistrationMapper.java
@@ -0,0 +1,20 @@
+package com.mgmtp.cfu.mapper;
+
+import com.mgmtp.cfu.DTO.RegistrationDTO;
+import com.mgmtp.cfu.entity.Registration;
+public class RegistrationMapper {
+    public static RegistrationDTO toDto(Registration registration) {
+        if(registration == null) {
+            return null;
+        }
+        return RegistrationDTO.builder()
+                .id(registration.getId())
+                .score(registration.getScore())
+                .registerDate(registration.getRegisterDate())
+                .duration(registration.getDuration())
+                .endDate(registration.getEndDate())
+                .startDate(registration.getStartDate())
+                .status(registration.getStatus())
+                .build();
+    }
+}
