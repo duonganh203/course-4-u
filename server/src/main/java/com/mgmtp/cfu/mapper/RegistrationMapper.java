e055eac3a324567ba09c6d15ff166e77e9c7fb70
Duong Anh <rankill52@gmail.com>
2024-07-01 13:38:45 +0700

    MGMIDN-1181: remove unnecessary condition

--- server/src/main/java/com/mgmtp/cfu/mapper/RegistrationMapper.java
+++ server/src/main/java/com/mgmtp/cfu/mapper/RegistrationMapper.java
@@ -4,9 +4,6 @@ import com.mgmtp.cfu.DTO.RegistrationDTO;
 import com.mgmtp.cfu.entity.Registration;
 public class RegistrationMapper {
     public static RegistrationDTO toDto(Registration registration) {
-        if(registration == null) {
-            return null;
-        }
         return RegistrationDTO.builder()
                 .id(registration.getId())
                 .score(registration.getScore())
